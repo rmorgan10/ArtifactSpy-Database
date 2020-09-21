8a92cec21718cc86d4f3e3e6180881674bef384d
Robert Morgan <robert.morgan@wisc.edu>
2020-09-21 15:10:05 -0500

    add functionality for overwriting labels

--- generate_training_set.py
+++ generate_training_set.py
@@ -14,9 +14,15 @@ import pandas as pd
 from utils import format_batchnum
 
 # Collect all labels (metadata already in master_labels)
-master_labels = pd.read_csv('Results/master_results.csv').set_index("OBJID")
-my_labels = pd.read_csv('Results/ramorgan2/my_labels.csv').set_index("OBJID")
-unsure_labels = pd.concat([pd.read_csv(x) for x in glob.glob('Results/UnsureResults/*.csv')]).set_index("OBJID")
+master_labels = pd.read_csv('Results/master_results.csv')
+master_labels['OBJID_COPY'] = master_labels['OBJID'].values
+master_labels.set_index("OBJID", inplace=True)
+my_labels = pd.read_csv('Results/ramorgan2/my_labels.csv')
+my_labels['OBJID_COPY'] = my_labels['OBJID'].values
+my_labels.set_index("OBJID", inplace=True)
+unsure_labels = pd.concat([pd.read_csv(x) for x in glob.glob('Results/UnsureResults/*.csv')])
+unsure_labels['OBJID_COPY'] = unsure_labels['OBJID'].values
+unsure_labels.set_index("OBJID", inplace=True)
 
 ### Overwrite unsure and overlapping labels
 my_labels['ACTION'] = my_labels['LABEL'].values
@@ -29,11 +35,18 @@ del unsure_labels, my_labels
 ### drop unnecessary columns
 master_labels.drop(['Unnamed: 0', 'METADATA_STAMP'], axis=1, inplace=True)
 
+### Overwrites from secondary inspection
+inspect_df = pd.read_csv('overwrite.csv', comment='#')
+inspect_df['OBJID_COPY'] = inspect_df['OBJID'].values
+inspect_df.set_index("OBJID", inplace=True)
+master_labels.update(inspect_df)
+
 ### Collect all batch numbers
 batch_objid_map = pd.read_csv('batch_objid_map.csv')
 batch_objid_map.drop_duplicates(inplace=True)
 batch_objid_map.set_index('OBJID', inplace=True)
 master_labels = master_labels.join(batch_objid_map, on='OBJID', how='inner')
+
 
 # Prepare data storage objects
 class_map = {'Good': 0,
@@ -48,6 +61,7 @@ class DataObject():
     def __init__(self):
         self.objid_list = []
         self.images = []
+        self.metadata_list = []
         return
 
 data = {v: DataObject() for v in class_map.values()}
@@ -73,9 +87,31 @@ for batch, labels in batch_labels:
 
     # Sort data
     for objid in valid_objids:
+        # Grab metadata
+        metadata_df = master_labels.loc[objid]
+        # Make sure we get one metadata entry per objid
+        
+        if isinstance(metadata_df, pd.Series):
+            metadata_df = pd.DataFrame(data=[metadata_df.values], columns=metadata_df.index.values)
+        else:
+            metadata_df = pd.DataFrame(data=[metadata_df.iloc[0].values], columns=metadata_df.iloc[0].index.values)
+
+        if metadata_df.shape[0] != 1:
+            continue        
+
         # Skip any lingering UNSURE images
-        if labels.loc[objid]['ACTION'] not in class_map.keys():
-            continue
+        try:
+            if labels.loc[objid]['ACTION'] not in class_map.keys():
+                continue
+            else:
+                action = labels.loc[objid]['ACTION']
+        except TypeError:
+            action = ''
+            for act in labels.loc[objid]['ACTION'].values:
+                if act in class_map.keys():
+                    action = act
+                    break
+            if action == '': continue
 
         # Isolate images
         prefix = batch_filename.split('/')[-1][:-7] + '/'
@@ -84,14 +120,25 @@ for batch, labels in batch_labels:
         diff = np.array(Image.open(t.extractfile(prefix + "diff" + str(objid) + ".gif")))
 
         # Organize using the data storage objects
-        data[class_map[labels.loc[objid]['ACTION']]].objid_list.append(objid)
-        data[class_map[labels.loc[objid]['ACTION']]].images.append(np.array([srch, temp, diff]))
+        #data[class_map[labels.loc[objid]['ACTION']]].objid_list.append(objid)
+        #data[class_map[labels.loc[objid]['ACTION']]].images.append(np.array([srch, temp, diff]))
+        #data[class_map[labels.loc[objid]['ACTION']]].metadata_list.append(metadata_df)
+
+        data[class_map[action]].objid_list.append(objid)
+        data[class_map[action]].images.append(np.array([srch, temp, diff]))
+        data[class_map[action]].metadata_list.append(metadata_df)
 
     t.close()
 
+# Checks
+for v in data.values():
+    assert len(v.metadata_list) == len(v.objid_list)
+    assert len(v.images) == len(v.objid_list)
+
 # Now attach the metadata
 for v in data.values():
-    v.metadata = master_labels.loc[v.objid_list]
+    #v.metadata = master_labels.loc[v.objid_list]
+    v.metadata = pd.concat(v.metadata_list)
 
 # Create a directory for the training set
 training_set_name = "TS__" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -100,7 +147,7 @@ os.mkdir("TrainingSets/" + training_set_name)
 # Save data
 for k, v in data.items():
     # Save metadata
-    v.metadata.to_csv("TrainingSets/" + training_set_name + '/Class_' + str(k) + '_metadata.csv')
+    v.metadata.to_csv("TrainingSets/" + training_set_name + '/Class_' + str(k) + '_metadata.csv', index=False)
 
     # Save OBJIDs
     listfile = open("TrainingSets/" + training_set_name + '/Class_' + str(k) + '_objids.txt', 'w+')
